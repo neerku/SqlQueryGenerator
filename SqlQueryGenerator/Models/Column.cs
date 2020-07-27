94527ba654ca36ea3961022930ea0af2aae8e2cd
Pooja Kumari <Poojak@touchcoresystems.com>
2020-07-27 16:17:49 +0530

    Added input json and model class..

---
+++ SqlQueryGenerator/Models/Column.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlQueryGenerator.Models
+{
+   public class Column
+    {
+        public string SqlOperator { get; set; }
+        public string FieldName { get; set; }
+        public string FieldValue { get; set; }
+    }
+}
---
+++ SqlQueryGenerator/Models/PrimaryTable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlQueryGenerator.Models
+{
+    public class PrimaryTable
+    {
+        public List<Column> Columns { get; set; }
+        public string Name { get; set; }
+    }
+}
